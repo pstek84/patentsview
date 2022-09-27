f98a3192a21c0ec8e31b82e299f7412479e15796
pstek84 <113000532+pstek84@users.noreply.github.com>
2022-09-27 11:55:51 +0800

    Update pv_location-geocode.R

--- pv_location-geocode.R
+++ pv_location-geocode.R
@@ -1,29 +1,21 @@
 ## PatentsView Location with Geocoder (Pelias)
 
-#.. download location zip
+#.. preamble
 setwd('~/patentsview') #Set working directory
-library(RMySQL); dbname <- 'dbname'; username <- 'username'; password <- 'password'; host <- 'localhost'
+library(RMySQL); dbname <- 'patentsview'; username <- 'user'; password <- 'password'; host <- 'localhost'
 dl_url <- 'https://s3.amazonaws.com/data.patentsview.org/download/' #Set generic URL with PatentsView data
 
-fname <- 'location.tsv' #File name
-download.file(paste0(dl_url, fname, '.zip'), paste0(fname, '.zip')) #Download file
+#.. download
+tname <- 'location' #File name
+download.file(paste0(dl_url, tname, '.tsv.zip'), paste0(fname, '.tsv.zip')) #Download file
 unzip(paste0(fname, '.zip')) #Unzip
-data <- read.csv(fname, sep='\t')
 
-#.. write to location table in database
+#.. load data and amend table
 con <- dbConnect(RMySQL::MySQL(), dbname= dbname, username= username, password= password, host= host)
 #dbListTables(con)
 #dbRemoveTable(con, 'location')
 dbSendQuery(con, 'CREATE TABLE location (id VARCHAR(128), city VARCHAR(128), state VARCHAR(2), country VARCHAR(2), latitude FLOAT, longitude FLOAT, county VARCHAR(60), state_fips VARCHAR(2), county_fips VARCHAR(6));')
-dbSendQuery(con, 'CREATE INDEX id ON location (id);')
-dbSendQuery(con, 'CREATE INDEX country ON location (country);')
-#dbSendQuery(con, 'SET GLOBAL local_infile=1;') #may have to do this as sudo user in MySQL command line
-dbWriteTable(con, 'location', data, row.names= FALSE, overwrite= FALSE, append= TRUE)
-#check <- dbReadTable(con, 'location')
-dbDisconnect(con); remove(con)
-
-#.. update location table structure (add columns)
-con <- dbConnect(RMySQL::MySQL(), dbname= dbname, username= username, password= password, host= host)
+dbSendQuery(con, 'LOAD DATA LOCAL INFILE "/home/user/patentsview/location.tsv"  INTO TABLE location FIELDS TERMINATED BY "\t" ENCLOSED BY "\\"" LINES TERMINATED BY "\n" IGNORE 1 LINES;')
 dbSendQuery(con, 'ALTER TABLE location ADD COLUMN lat2 FLOAT,
             ADD COLUMN lon2 FLOAT,
             ADD COLUMN name VARCHAR(128),
@@ -35,6 +27,7 @@ dbDisconnect(con); remove(con)
 #.. geocoding
 library(jsonlite)
 gc_url <- 'http://localhost:3000/parser/search?text=' #URL for geocoding via Pelias, see: https://geocode.earth/blog/2019/almost-one-line-coarse-geocoding/
+# "Turn on" in CMD: docker run -d -p 3000:3000 -v $(pwd)/data:/data pelias/placeholder
 
 con <- dbConnect(RMySQL::MySQL(), dbname= dbname, username= username, password= password, host= host)
 ids <- dbFetch(dbSendQuery(con, 'SELECT id FROM location;'), n= -1) #select id list
@@ -61,5 +54,12 @@ for(n in ids$id){ #start with ids, use ids2 if crashed
 #which(ids$id == n) #on crash, check which ID
 #ids2 <- as.data.frame(ids[which(ids$id == n):nrow(ids),]); names(ids2) <- 'id' #on crash, shorten ids list
 dbDisconnect(con); remove(con)
+
+#.. add indexes
+con <- dbConnect(RMySQL::MySQL(), dbname= dbname, username= username, password= password, host= host)
+dbSendQuery(con, 'CREATE INDEX id ON location (id);')
+dbSendQuery(con, 'CREATE INDEX country ON location (country);')
+dbDisconnect(con); remove(con)
+
 
 
